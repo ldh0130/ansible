424a7330be67fd100c3b4583be58f4ba92779f72
ldh5131 <ldh5131@naver.com>
2023-03-24 04:44:15 -0400

    2nd commit

---
+++ 00_bin/known_hosts_update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash $(cat $HOME/bin/host.list)
+do
+	#echo $h
+		sshpass -p ansible ssh -oStrictHostKeyChecking=no ansible@$h 'exit;'
+		done
+		
+#!/bin/bash
+
+for h in $(cat $HOME/bin/host.list)
+do
+	#echo $h
+	sshpass -p ansible ssh -oStrictHostKeyChecking=no ansible@$h 'exit;'
+done
---
+++ 07_template-motd/restore_motd.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ansible all -m shell -a '> /etc/motd'
+ansible all -m shell -a 'ls -l /etc/motd'
+
